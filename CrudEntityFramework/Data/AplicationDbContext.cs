125bfe61bc7701627f29480cfd105cacfa1f7e05
jcamilovallejos@gmail.com <jcamilovallejos@gmail.com>
2020-03-28 14:05:55 -0500

    Primer commit Crud

---
+++ CrudEntityFramework/Data/AplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CrudEntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudEntityFramework.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
